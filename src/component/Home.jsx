62353d9ddfdb9304422abe6ffc492880e3c95731
wahyudwiprstka <wahyudwiprstka@gmail.com>
2023-01-26 16:00:51 +0800

    redirect button

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
                     <h1 className="mt-2 text-4xl font-bold text-slate-800">Hi! I am <br/> <span className="text-sky-700">Wahyu Dwi Prastika</span></h1>
                     <h3 className="mt-2 text-2xl font-semibold text-slate-600">Web Developer</h3>
                     <p className="mt-2 text-lg">A Fresh Graduate Fullstack Web Developer from Bali, Indonesia.</p>
-                    <a><button className="px-3 py-2 bg-indigo-600 rounded-lg shadow mt-2 text-white hover:bg-indigo-800 font-medium transition duration-150">My Profile Video</button></a>
+                    <a href="https://www.youtube.com/watch?v=5nMvgkY-qMw"><button className="px-3 py-2 bg-indigo-600 rounded-lg shadow mt-2 text-white hover:bg-indigo-800 font-medium transition duration-150">My Profile Video</button></a>
                 </div>
                 <div className='w-full relative lg:w-1/2'>
                     <img src={myPhoto} alt="My Photo" className='m-auto'/>
